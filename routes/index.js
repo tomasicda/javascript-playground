f1b708bd6506de6725ada833d220f8127411e4f9
Daniel Tomasic <tomasicda@gmail.com>
2018-04-15 22:48:19 +1000

    First push, few different exercies here

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/task2', function(req, res, next) {
+  res.render('task2', { title: 'Express' });
+});
+
+router.get('/task3', function(req, res, next) {
+  res.render('task3', { title: 'Express' });
+});
+
+router.get('/task4', function(req, res, next) {
+  res.render('task4', { title: 'Express' });
+});
+
+
+
+module.exports = router;
